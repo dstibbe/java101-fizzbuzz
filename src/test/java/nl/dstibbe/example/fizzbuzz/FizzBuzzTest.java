534f0e2f9bf3e42117f9afd98d591b020e96f0f3
David Stibbe <dstibbe@gmail.com>
2017-10-23 23:48:59 +0200

    Added JUnit tests

---
+++ src/test/java/nl/dstibbe/example/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,20 @@
+package nl.dstibbe.example.fizzbuzz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+//    @Test
+//    public void varShouldEqualTwo(){
+//        int myValue = 2;
+//        Assert.assertEquals("myValue does not match expectations", 3, myValue);
+//    }
+
+//    @Test
+//    public void round5ShouldBeBuzz(){
+//        FizzBuzz game = new FizzBuzz();
+//        Assert.assertEquals("Incorrect result", "Buzz", game.play(5));
+//    }
+
+}
